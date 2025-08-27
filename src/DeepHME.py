1bb76ca5d5800a8b85b4c7308de11c3a26892134
Artem Bolshov <abolshov@cern.ch>
2025-08-27 17:07:30 +0200

    start implementation of DeepHME

---
+++ src/DeepHME.py
@@ -0,0 +1,14 @@
+import numpy as np
+import awkward as ak
+import vector
+import onnxruntime as ort
+import yaml
+import os
+
+class DeepHME:
+    def __init__(self):
+        pass
+
+    def predict(self):
+        pass
+    
