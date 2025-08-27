cf7a13751fd8889bd288b4d573d9e044f4bec419
Artem Bolshov <abolshov@cern.ch>
2025-08-27 17:24:24 +0200

    loading model params

--- src/DeepHME.py
+++ src/DeepHME.py
@@ -6,9 +6,13 @@ import yaml
 import os
 
 class DeepHME:
-    def __init__(self):
-        pass
+    def __init__(self, model_name):
+        self._base_model_dir = 'models'
+        self._model_dir = os.path.join(self._base_model_dir, model_name)
+        self._train_cfg = {}
+        with open(os.path.join(self._model_dir, 'params_model.yaml'), 'r') as train_cfg_file:
+            self._train_cfg = yaml.safe_load(train_cfg_file)
 
     def predict(self):
-        pass
+        print(self._train_cfg)
     
